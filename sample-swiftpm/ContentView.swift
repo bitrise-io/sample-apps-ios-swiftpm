627c8792bc2365d8a8dd7588ce6eb07e705f4125
lpusok <7979773+lpusok@users.noreply.github.com>
2019-09-23 10:42:42 +0200

    TOOL-1115 Adding sample project with swift PM (#1)
    * Adding project

---
+++ sample-swiftpm/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  sample swiftpm
+//
+//  Created by lpusok on 2019. 09. 17..
+//  Copyright © 2019. bitrise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-swiftpmTests/sample_swiftpmTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_swiftpmTests.swift
+//  sample swiftpmTests
+//
+//  Created by lpusok on 2019. 09. 17..
+//  Copyright © 2019. bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import sample_swiftpm
+
+class sample_swiftpmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
